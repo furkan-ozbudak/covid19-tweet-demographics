f0494dc28aa1af44e43c13f5793e2e36454288eb
Furkan Ozbudak <51862409+furkan-ozbudak@users.noreply.github.com>
2020-10-18 14:58:53 -0500

    finalized

--- SparkProducer/src/main/java/twitter_kafka/TwitterConfiguration.java
+++
@@ -1,8 +0,0 @@
-package twitter_kafka;
-public class TwitterConfiguration {
-    public static final String CONSUMER_KEY = "kwVxyctIXFzNHs0E8UhgXfl5g";
-    public static final String CONSUMER_SECRET = "tHZ55ExNnIC93KGdOQG3Cg4SX5b15biaoMcLjpC00fFu0M7zku";
-    public static final String ACCESS_TOKEN = "1291762318876053509-12thWgZUW2N5ZDjSGHLG3Bu5r5fd26";
-    public static final String ACCESS_TOKEN_SECRET = "e2pw5H41mtc148uXHZhmCDA1lmPfTCoGB3zcOEobBNuS9";
-    public static final String HASHTAG = "#coronavirus,#COVID19";
-}
